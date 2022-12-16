4cbcdba7ccee06afafe160067cad7338421a4150
Yi Yao <leaf_yao8621@hotmail.com>
2022-12-15 17:25:42 -0800

    darray

--- include/containers/darray.h
+++ include/containers/darray.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include <containers/errcode.h>
 
 #define DEF_DARRAY(Type)\
@@ -11,7 +12,12 @@ typedef struct DArray##Type {\
     Type *data;\
 } DArray##Type;\
 int DArray##Type##_initialize(DArray##Type *darray, size_t capacity);\
-int DArray##Type##_finalize(DArray##Type *darray);
+int DArray##Type##_finalize(DArray##Type *darray);\
+int DArray##Type##_push_back(DArray##Type *darray, Type *item);\
+int DArray##Type##_push_back_batch(\
+    DArray##Type *darray,\
+    Type *items,\
+    size_t len);
 
 #define DEF_DARRAY_FUNCTIONS(Type)\
 int DArray##Type##_initialize(DArray##Type *darray, size_t capacity) {\
@@ -31,6 +37,38 @@ int DArray##Type##_finalize(DArray##Type *darray) {\
         return ERR_NULL_PTR;\
     }\
     free(darray->data);\
+    return ERR_OK;\
+}\
+int DArray##Type##_push_back(DArray##Type *darray, Type *item) {\
+    if (!darray || !item) {\
+        return ERR_NULL_PTR;\
+    }\
+    if (darray->size == darray->capacity) {\
+        darray->capacity <<= 1;\
+        darray->data = realloc(darray->data, sizeof(Type) * darray->capacity);\
+        if (!darray->data) {\
+            return ERR_INSUFFICIENT_MEMORY;\
+        }\
+    }\
+    darray->data[darray->size++] = *item;\
+    return ERR_OK;\
+}\
+int DArray##Type##_push_back_batch(\
+    DArray##Type *darray,\
+    Type *items,\
+    size_t len) {\
+    if (!darray || !items) {\
+        return ERR_NULL_PTR;\
+    }\
+    if (darray->size + len > darray->capacity) {\
+        darray->capacity = darray->size + len;\
+        darray->data = realloc(darray->data, sizeof(Type) * darray->capacity);\
+        if (!darray->data) {\
+            return ERR_INSUFFICIENT_MEMORY;\
+        }\
+    }\
+    memcpy(darray->data + darray->size, items, sizeof(Type) * len);\
+    darray->size += len;\
     return ERR_OK;\
 }
 
--- src/test.c
+++ src/test.c
@@ -3,14 +3,31 @@
 
 #define REPORT printf("%d %s\n", ret, containers_errcode_lookup[ret]);
 
-typedef const char *String;
+typedef char *String;
 DEF_DARRAY(String)
 DEF_DARRAY_FUNCTIONS(String)
 
 int main(void) {
     DArrayString darray;
-    int ret = DArrayString_initialize(&darray, 10);
+    int ret = DArrayString_initialize(&darray, 2);
     REPORT
+    char *buf[] = {
+        "0",
+        "1",
+        "2",
+        "3",
+        "4"
+    };
+    for (int i = 0; i < 5; ++i) {
+        ret = DArrayString_push_back(&darray, buf + i);
+        REPORT
+    }
+    ret = DArrayString_push_back_batch(&darray, buf, 5);
+    REPORT
+    printf("%lu %lu\n", darray.size, darray.capacity);
+    for (int i = 0; i < 10; ++i) {
+        printf("%s\n", darray.data[i]);
+    }
     ret = DArrayString_finalize(&darray);
     REPORT
     return 0;
