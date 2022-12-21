8474c2acf75a9eb3745098b1ee2869673eaf90f0
Yi Yao <leaf_yao8621@hotmail.com>
2022-12-21 15:01:01 -0800

    hashmap fetch

---
+++ include/containers/hashmap.h
@@ -0,0 +1,106 @@
+#ifndef CONTAINERS_HASHMAP_H_
+#define CONTAINERS_HASHMAP_H_
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <containers/errcode.h>
+
+#define DEF_HASHMAP(Key, Value)\
+typedef struct HashMap##Key##Value##Node {\
+    bool in_use;\
+    Key key;\
+    Value value;\
+} HashMap##Key##Value##Node;\
+typedef struct HashMap##Key##Value {\
+    size_t size, capacity;\
+    HashMap##Key##Value##Node *data;\
+    bool (*eq)(Key*, Key*);\
+    size_t (*hash)(Key*);\
+} HashMap##Key##Value;\
+int HashMap##Key##Value##_initialize(\
+    HashMap##Key##Value *hashmap,\
+    size_t capacity,\
+    bool (*eq)(Key*, Key*),\
+    size_t (*hash)(Key*));\
+int HashMap##Key##Value##_finalize(HashMap##Key##Value *hashmap);\
+int HashMap##Key##Value##_fetch(\
+    HashMap##Key##Value *hashmap,\
+    Key *key,\
+    Value **value);\
+int HashMap##Key##Value##_find(\
+    HashMap##Key##Value *hashmap,\
+    Key *key,\
+    bool *found);
+
+#define DEF_HASHMAP_FUNCTIONS(Key, Value)\
+int HashMap##Key##Value##_initialize(\
+    HashMap##Key##Value *hashmap,\
+    size_t capacity,\
+    bool (*eq)(Key*, Key*),\
+    size_t (*hash)(Key*)) {\
+    if (!hashmap || !eq || !hash) {\
+        return CONTAINERS_ERR_NULL_PTR;\
+    }\
+    hashmap->size = 0;\
+    hashmap->capacity = capacity;\
+    hashmap->data = calloc(capacity, sizeof(HashMap##Key##Value##Node));\
+    if (!hashmap->data) {\
+        return CONTAINERS_ERR_INSUFFICIENT_MEMORY;\
+    }\
+    hashmap->eq = eq;\
+    hashmap->hash = hash;\
+    return CONTAINERS_ERR_OK;\
+}\
+int HashMap##Key##Value##_finalize(HashMap##Key##Value *hashmap) {\
+    if (!hashmap) {\
+        return CONTAINERS_ERR_NULL_PTR;\
+    }\
+    free(hashmap->data);\
+    return CONTAINERS_ERR_OK;\
+}\
+int HashMap##Key##Value##_fetch(\
+    HashMap##Key##Value *hashmap,\
+    Key *key,\
+    Value **value) {\
+    if (!hashmap || !key || !value) {\
+        return CONTAINERS_ERR_NULL_PTR;\
+    }\
+    if (hashmap->size == hashmap->capacity) {\
+        hashmap->capacity <<= 1;\
+        HashMap##Key##Value##Node *buf =\
+            calloc(hashmap->capacity, sizeof(HashMap##Key##Value##Node));\
+        if (!buf) {\
+            return CONTAINERS_ERR_INSUFFICIENT_MEMORY;\
+        }\
+        HashMap##Key##Value##Node *iter = hashmap->data;\
+        for (size_t i = 0; i < hashmap->size; ++i, ++iter) {\
+            size_t idx = hashmap->hash(&iter->key) % hashmap->capacity;\
+            for (\
+                ;\
+                buf[idx].in_use;\
+                idx = (idx + 1) % hashmap->capacity);\
+            buf[idx].in_use = true;\
+            buf[idx].key = iter->key;\
+            buf[idx].value = iter->value;\
+        }\
+        free(hashmap->data);\
+        hashmap->data = buf;\
+    }\
+    size_t idx = hashmap->hash(key) % hashmap->capacity;\
+    size_t cnt = 0;\
+    for (\
+        ;\
+        cnt < hashmap->capacity &&\
+        hashmap->data[idx].in_use &&\
+        !hashmap->eq(&hashmap->data[idx].key, key);\
+        idx = (idx + 1) % hashmap->capacity);\
+    if (!hashmap->data[idx].in_use) {\
+        ++hashmap->size;\
+        hashmap->data[idx].in_use = true;\
+        hashmap->data[idx].key = *key;\
+    }\
+    *value = &hashmap->data[idx].value;\
+    return CONTAINERS_ERR_OK;\
+}
+
+#endif
--- src/test.c
+++ src/test.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <containers/darray.h>
 #include <containers/hashset.h>
+#include <containers/hashmap.h>
 #include <containers/eq.h>
 #include <containers/hash.h>
 
 #define REPORT printf("%d %s\n", ret, containers_errcode_lookup[ret]);
 
 typedef char *String;
+typedef int Int;
 DEF_DARRAY(String)
 DEF_DARRAY_FUNCTIONS(String)
 DEF_HASHSET(String)
 DEF_HASHSET_FUNCTIONS(String)
+DEF_HASHMAP(String, Int)
+DEF_HASHMAP_FUNCTIONS(String, Int)
 
 int main(void) {
     DArrayString darray;
@@ -40,7 +44,8 @@ int main(void) {
         "adqwe124",
         "adqwe124",
         "qwertttt123",
-        "adqwe1245"
+        "adqwe1245",
+        "xxhsaklfjsalfksadljklasd"
     };
     HashSetString hashset;
     ret =
@@ -51,13 +56,13 @@ int main(void) {
             containers_hash_str
         );
     REPORT
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 6; ++i) {
         ret = HashSetString_insert(&hashset, buf_hs + i);
         REPORT
     }
     for (size_t i = 0; i < hashset.capacity; ++i) {
         printf(
-            "in_use: %hhd value: %s\n",
+            "in_use: %hhd data: %s\n",
             hashset.data[i].in_use,
             hashset.data[i].data
         );
@@ -67,15 +72,55 @@ int main(void) {
         "adqwe124",
         "adqwe1243",
         "qwertttt123",
-        "adqwe1245"
+        "adqwe1245",
+        "ssqkl"
     };
-    for (size_t i = 0; i < 5; ++i) {
+    for (size_t i = 0; i < 6; ++i) {
         bool found = false;
         ret = HashSetString_find(&hashset, &buf_hs_find[i], &found);
         REPORT
         printf("found: %hhd\n", found);
     }
     ret = HashSetString_finalize(&hashset);
+    REPORT
+    int buf_val[] = {
+        0,
+        1,
+        2,
+        3,
+        4,
+        5
+    };
+    HashMapStringInt hashmap;
+    ret =
+        HashMapStringInt_initialize(
+            &hashmap,
+            2,
+            containers_eq_str,
+            containers_hash_str
+        );
+    REPORT
+    for (int i = 0; i < 6; ++i) {
+        int *res = 0;
+        ret = HashMapStringInt_fetch(&hashmap, buf_hs + i, &res);
+        REPORT
+        *res = buf_val[i];
+    }
+    for (size_t i = 0; i < hashmap.capacity; ++i) {
+        printf(
+            "in_use: %hhd key: %s value: %d\n",
+            hashmap.data[i].in_use,
+            hashmap.data[i].key,
+            hashmap.data[i].value
+        );
+    }
+    // for (size_t i = 0; i < 5; ++i) {
+    //     bool found = false;
+    //     ret = HashMapStringInt_find(&hashmap, &buf_hs_find[i], &found);
+    //     REPORT
+    //     printf("found: %hhd\n", found);
+    // }
+    ret = HashMapStringInt_finalize(&hashmap);
     REPORT
     return 0;
 }
