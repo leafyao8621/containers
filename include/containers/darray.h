6133b8476f4db834cf830f1adbfbe46b4ace7571
Yi Yao <leaf_yao8621@hotmail.com>
2022-12-21 11:35:27 -0800

    namespace

--- include/containers/darray.h
+++ include/containers/darray.h
@@ -22,54 +22,54 @@ int DArray##Type##_push_back_batch(\
 #define DEF_DARRAY_FUNCTIONS(Type)\
 int DArray##Type##_initialize(DArray##Type *darray, size_t capacity) {\
     if (!darray) {\
-        return ERR_NULL_PTR;\
+        return CONTAINERS_ERR_NULL_PTR;\
     }\
     darray->data = malloc(sizeof(Type) * capacity);\
     if (!darray->data) {\
-        return ERR_INSUFFICIENT_MEMORY;\
+        return CONTAINERS_ERR_INSUFFICIENT_MEMORY;\
     }\
     darray->size = 0;\
     darray->capacity = capacity;\
-    return ERR_OK;\
+    return CONTAINERS_ERR_OK;\
 }\
 int DArray##Type##_finalize(DArray##Type *darray) {\
     if (!darray) {\
-        return ERR_NULL_PTR;\
+        return CONTAINERS_ERR_NULL_PTR;\
     }\
     free(darray->data);\
-    return ERR_OK;\
+    return CONTAINERS_ERR_OK;\
 }\
 int DArray##Type##_push_back(DArray##Type *darray, Type *item) {\
     if (!darray || !item) {\
-        return ERR_NULL_PTR;\
+        return CONTAINERS_ERR_NULL_PTR;\
     }\
     if (darray->size == darray->capacity) {\
         darray->capacity <<= 1;\
         darray->data = realloc(darray->data, sizeof(Type) * darray->capacity);\
         if (!darray->data) {\
-            return ERR_INSUFFICIENT_MEMORY;\
+            return CONTAINERS_ERR_INSUFFICIENT_MEMORY;\
         }\
     }\
     darray->data[darray->size++] = *item;\
-    return ERR_OK;\
+    return CONTAINERS_ERR_OK;\
 }\
 int DArray##Type##_push_back_batch(\
     DArray##Type *darray,\
     Type *items,\
     size_t len) {\
     if (!darray || !items) {\
-        return ERR_NULL_PTR;\
+        return CONTAINERS_ERR_NULL_PTR;\
     }\
     if (darray->size + len > darray->capacity) {\
         darray->capacity = darray->size + len;\
         darray->data = realloc(darray->data, sizeof(Type) * darray->capacity);\
         if (!darray->data) {\
-            return ERR_INSUFFICIENT_MEMORY;\
+            return CONTAINERS_ERR_INSUFFICIENT_MEMORY;\
         }\
     }\
     memcpy(darray->data + darray->size, items, sizeof(Type) * len);\
     darray->size += len;\
-    return ERR_OK;\
+    return CONTAINERS_ERR_OK;\
 }
 
 #endif
--- include/containers/errcode.h
+++ include/containers/errcode.h
@@ -1,9 +1,9 @@
 #ifndef CONTAINERS_ERRCODE_H_
 #define CONTAINERS_ERRCODE_H_
 
-#define ERR_OK 0
-#define ERR_NULL_PTR 1
-#define ERR_INSUFFICIENT_MEMORY 2
+#define CONTAINERS_ERR_OK 0
+#define CONTAINERS_ERR_NULL_PTR 1
+#define CONTAINERS_ERR_INSUFFICIENT_MEMORY 2
 
 extern const char *containers_errcode_lookup[3];
 
