080345fd14cdc703cb24c07275e61bd471d439d8
Yi Yao <leaf_yao8621@hotmail.com>
2023-05-01 17:12:56 -0700

    Updates

--- include/containers/hashmap.h
+++ include/containers/hashmap.h
@@ -115,8 +115,7 @@ int HashMap##Key##Value##_find(\
         hashmap->data[idx].in_use &&\
         !hashmap->eq(&hashmap->data[idx].key, key);\
         ++cnt, idx = (idx + 1) % hashmap->capacity);\
-    *found =\
-        hashmap->data[idx].in_use && cnt < hashmap->capacity;\
+    *found = hashmap->data[idx].in_use && cnt < hashmap->capacity;\
     return CONTAINERS_ERR_OK;\
 }
 
